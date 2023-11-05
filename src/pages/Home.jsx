154de5fe5d574ca872637f223c573d8889c433f5
Amaymani <amaytripathi19@gmail.com>
2023-11-05 15:07:29 +0530

    adding files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../index.css";
+import Landing from "../Components/Landing";
+import Navbar from "../Components/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <Landing/>
+    </div>
+  );
+}
+export default Home;
