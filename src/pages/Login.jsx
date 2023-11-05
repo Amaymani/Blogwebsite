39c3f51bda907f7d27fa87c7c0e6067353b72f35
Amaymani <amaytripathi19@gmail.com>
2023-11-05 15:38:59 +0530

    login page added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,29 +5,46 @@ import Navbar from "../Components/Navbar";
 function Login() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
 
       <form class="login-div">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+        <h1 class="h3 mb-3 fw-normal">Please Log in</h1>
 
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
-      <label for="floatingPassword">Password</label>
-    </div>
+        <div class="form-floating">
+          <input
+            type="email"
+            class="form-control login-input"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">Username</label>
+        </div>
+        <div class="form-floating">
+          <input
+            type="password"
+            class="form-control login-input"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
 
-    <div class="form-check text-start my-3">
-      <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault"/>
-      <label class="form-check-label" for="flexCheckDefault">
-        Remember me
-      </label>
-    </div>
-    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
-  </form>
+        <div class="form-check text-start my-3">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            value="remember-me"
+            id="flexCheckDefault"
+          />
+          <label class="form-check-label" for="flexCheckDefault">
+            Remember me
+          </label>
+        </div>
+        <button class="btn login-btn btn-primary w-100 py-2" type="submit">
+          Log in
+        </button>
+        <p class="mt-5 mb-3 text-body-secondary">© Dr.Paradox</p>
+      </form>
     </div>
   );
 }
